5318f16ec398ca65922813aa2d07697168753a94
Gordon Caister <gordoncaister@gmail.com>
2020-06-10 15:32:31 -0500

    all tests workign as intended for doubly linked lsit

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -91,13 +91,21 @@ class DoublyLinkedList:
     """Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
+
     def remove_from_tail(self):
-        if not self.head or not self.tail:
+        if not self.head and not self.tail:
             return None
-        else:
-            value = self.tail.value
-            self.delete(self.tail)
-            return value
+        self.length -= 1
+        if self.head == self.tail:
+            current_tail = self.tail
+            self.head = None
+            self.tail = None
+            return current_tail.value
+        current_tail = self.tail
+        self.tail = self.tail.prev
+        self.tail.next = None
+        return current_tail.value
+            
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
@@ -105,25 +113,23 @@ class DoublyLinkedList:
         if not self.tail or not self.head:
             return None
         elif self.head == self.tail:
+            self.length -= 1
             return None
         else:
-            self.delete(node)
-            self.add_to_head(node)
+            self.length -= 1
+            node.delete()
+            self.add_to_head(node.value)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        if node == self.tail:
-            return
-        elif node == self.head:
-            self.head = self.head.next
-            self.head.prev == None
-            self.tail.next = node
-            self.tail = node
-            self.tail.next = None
-        self.tail.next = node
-        self.tail = node
-        self.tail.next = None
+        if node == self.head:
+            self.remove_from_head()
+            self.add_to_tail(node.value)
+        else:
+            node.delete()
+            self.length -= 1
+            self.add_to_tail(node.value)
 
 
     """Removes a node from the list and handles cases where
